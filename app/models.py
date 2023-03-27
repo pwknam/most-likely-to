550904ce4dbfc1155f5f1047aacf104251e32728
Kyushik <kyushik95@gmail.com>
2023-03-27 19:00:07 -0400

    tables created. seed.py works

--- app/models.py
+++ app/models.py
@@ -9,29 +9,15 @@ Base = declarative_base()
 
 
 class User(Base):
-    # def __init__(self, username, password):
-    #    self._id = None
-    #    self._username = username
-    #    self._password = password
-
-    # @property
-    # def username(self):
-    #     return self._username
-    
-    # @property
-    # def password(self):
-    #     return self._password
-
-
+  
     __tablename__ = 'users'
 
     id = Column(Integer(), primary_key=True)
     username = Column(String())
     password = Column(String())
 
-    # superlatives_created = relationship('Superlative', backref=backref('user'))
+    superlatives_created = relationship('Superlative', backref=backref('user'))
     superlatives_voted = relationship('Votes', backref=backref('user'))
-    # user_nominations = relationship('Votes', backref=backref('user'))
 
 class Nominees(Base):
     __tablename__ = 'nominees'
@@ -44,24 +30,14 @@ class Nominees(Base):
 
 
 class Superlative(Base):
-    # def __init__(self, name):
-    #     self._id = None
-        
-    #     if isinstance(name, str):
-    #        self._name = name
-    #     else: print("Superlative must be a string")
-
-    # @property
-    # def name(self):
-    #     return self._name
 
     __tablename__ = 'superlatives'
 
     id = Column(Integer(),primary_key=True)
     name = Column(String())
-    # user_id = Column(Integer(), ForeignKey('users.id'))
+    author_id = Column(Integer(), ForeignKey('users.id'))
     date_created = Column(DateTime(), server_default=func.now())
-    # date_expired = Column(DateTime(), server_default=func.now() + 10000)
+    date_expired = Column(DateTime(), server_default=func.now())
 
     superlative_votes = relationship('Votes', backref=backref('superlative'))
 
@@ -69,24 +45,13 @@ class Superlative(Base):
 
 
 class Votes(Base):
-    # def __init__(self, superlative_id, candidate_id):
-    #     self._superlative_id = superlative_id
-    #     self._candidate_id = candidate_id
-    
-    # @property
-    # def superlative_id(self):
-    #     return self._superlative_id
-    
-    # @property
-    # def candidate_id(self):
-    #     return self._candidate_id
 
     __tablename__ = 'votes'
 
     id = Column(Integer(), primary_key=True)
     voter_id = Column(Integer(), ForeignKey('users.id'))
     superlative_id = Column(Integer(), ForeignKey('superlatives.id'))
-    candidate = Column(String(), ForeignKey('nominees.id'))
+    nominee_id = Column(Integer(), ForeignKey('nominees.id'))
     date_voted = Column(DateTime(), server_default=func.now())
 
 
--- app/seed.py
+++ app/seed.py
@@ -1,31 +1,66 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import User, Superlative, Votes
+from models import User, Nominees, Superlative, Votes
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///superlatives.db')
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # session.query(User).delete()
-    # session.query(Superlative).delete()
-    # session.query(Votes).delete()
+    session.query(User).delete()
+    session.query(Nominees).delete()
+    session.query(Superlative).delete()
+    session.query(Votes).delete()
     
     user_1 = User(username = "Kyushik", password = "pwkn")
     session.add(user_1)
     session.commit()
 
-    # user_2 = User("Sally", "sk")
-    # session.add(user_2)
-    # session.commit(user_2)
+    user_2 = User(username = "Michelle", password = "mc")
+    session.add(user_2)
+    session.commit()
 
-    # user_3 = User("Michelle", "mc")
-    # session.add(user_3)
-    # session.commit(user_3)
+    user_3 = User(username = "Sally", password = "sk")
+    session.add(user_3)
+    session.commit()
 
-    superlative_1 = Superlative(name = "Most likely to become president")
+    nominee_1 = Nominees(name = "Kyushik")
+    session.add(nominee_1)
+    session.commit()
+
+    nominee_2 = Nominees(name = "Michelle")
+    session.add(nominee_2)
+    session.commit()
+
+    nominee_3 = Nominees(name = "Sally")
+    session.add(nominee_3)
+    session.commit()
+
+
+    superlative_1 = Superlative(name = "Most likely to become president.", author_id = 1)
     session.add(superlative_1)
+    session.commit()
+
+    superlative_2 = Superlative(name = "Best dressed.", author_id = 2)
+    session.add(superlative_2)
+    session.commit()
+
+    superlative_3 = Superlative(name = "Smartest.", author_id = 3)
+    session.add(superlative_3)
+    session.commit()
+
+
+    vote_1 = Votes(voter_id = 1, superlative_id = 1, nominee_id = 3)
+    session.add(vote_1)
+    session.commit()
+
+    vote_2 = Votes(voter_id = 2, superlative_id = 2, nominee_id = 1)
+    session.add(vote_2)
+    session.commit()
+
+    vote_3 = Votes(voter_id = 3, superlative_id = 3, nominee_id = 2)
+    session.add(vote_3)
     session.commit()
 
 
