fea4fb079ec1a78b4b649cdd77d72ba21db06a60
Kyushik <kyushik95@gmail.com>
2023-03-28 19:51:29 -0400

    jason session

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ from simple_term_menu import TerminalMenu
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, func
+from datetime import datetime
 
 #models/classes
 from models import User, Nominees, Superlative, Votes
@@ -25,20 +27,49 @@ def type_superlative():
 
 
 def get_superlative():
-    print("getting superlative...")
+    click.echo("getting superlative...")
     superlative = session.query(Superlative.name).all()
     click.echo(superlative)
 
 def login():
     pass
 
+def view_user_polls_created():
+    click.echo("Getting Polls that this user created")
+
+    logged_in_user_id = 4
+    superlatives = session.query(Superlative).filter(Superlative.author_id == logged_in_user_id)
+    
+    options = []
+
+    for superlative in superlatives:
+        options.append(f'{superlative.name}')
+    options.append("***Go back to the homepage***")
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    print(options[menu_entry_index])
+    type_superlative()
+    # click.echo(f"Selection: {options[menu_entry_index]}!")
+
+
+def view_polls_user_not_voted():
+    click.echo("Getting polls you have not voted on yet")
+    click.echo(datetime.now())
+
+def print(value):
+    click.echo(value)
+
+
+
 
 
 #main function
 @click.command()
 def main():
-    type_superlative()
-    get_superlative()
+    # type_superlative()
+    # get_superlative()
+    view_user_polls_created()
+    # view_polls_user_not_voted()
 
 
 
@@ -46,10 +77,5 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///superlatives.db')
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    session.query(User).delete()
-    session.query(Nominees).delete()
-    session.query(Superlative).delete()
-    session.query(Votes).delete()
-
+  
     main()
--- app/seed.py
+++ app/seed.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
 from models import User, Nominees, Superlative, Votes
 
@@ -10,6 +11,8 @@ if __name__ == '__main__':
 
     session.query(User).delete()
     session.query(Nominees).delete()
+    session.query(Superlative).delete()
+    session.query(Votes).delete()
 
     people = [
         {'username': "nick", 'password': 'n'},
@@ -40,6 +43,68 @@ if __name__ == '__main__':
         nominee = Nominees(name = person['username'])
         session.add(nominee)
         session.commit()
+
+    date_expired_str = '2023-04-8'
+    date_expired_format = datetime.strptime(date_expired_str, '%Y-%m-%d')
+    superlative_1 = Superlative(name = "Best Dressed", author_id = 4, date_expired = date_expired_format)
+    session.add(superlative_1)
+    session.commit()
+
+    superlative_2 = Superlative(name = "Most handsom", author_id = 5, date_expired = date_expired_format)
+    session.add(superlative_2)
+    session.commit()
+
+    superlative_3 = Superlative(name = "Most beautiful", author_id = 8, date_expired = date_expired_format)
+    session.add(superlative_3)
+    session.commit()
+
+    superlative_4 = Superlative(name = "Most likely to become president.", author_id = 4, date_expired = date_expired_format)
+    session.add(superlative_4)
+    session.commit()
+
+    superlative_5 = Superlative(name = "Most likely to become rich", author_id = 4, date_expired = date_expired_format)
+    session.add(superlative_5)
+    session.commit()
+
+    superlative_6 = Superlative(name = "Most likely to become a doctor", author_id = 4, date_expired = date_expired_format)
+    session.add(superlative_6)
+    session.commit()
+
+    vote_1 = Votes(voter_id = 4, superlative_id = 2, nominee_id = 5)
+    session.add(vote_1)
+    session.commit()
+
+    vote_2 = Votes(voter_id = 8, superlative_id = 2, nominee_id = 5)
+    session.add(vote_2)
+    session.commit()
+
+    vote_3 = Votes(voter_id = 3, superlative_id = 2, nominee_id = 5)
+    session.add(vote_3)
+    session.commit()
+
+    vote_4 = Votes(voter_id = 4, superlative_id = 2, nominee_id = 5)
+    session.add(vote_4)
+    session.commit()
+
+    vote_5 = Votes(voter_id = 2, superlative_id = 3, nominee_id = 1)
+    session.add(vote_5)
+    session.commit()
+
+    vote_6 = Votes(voter_id = 1, superlative_id = 3, nominee_id = 1)
+    session.add(vote_6)
+    session.commit()
+
+    vote_7 = Votes(voter_id = 9, superlative_id = 3, nominee_id = 1)
+    session.add(vote_7)
+    session.commit()
+
+    vote_8 = Votes(voter_id = 7, superlative_id = 3, nominee_id = 1)
+    session.add(vote_8)
+    session.commit()
+
+    
+
+
 
 
 
