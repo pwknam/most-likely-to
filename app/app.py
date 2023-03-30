ddc07330758fd008442e31aa2d3f7e99780eabe9
Kyushik <kyushik95@gmail.com>
2023-03-29 22:24:09 -0400

    michelle wednesday night

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ from tabulate import tabulate
 from models import User, Nominees, Superlative, Votes
 
 #global variables
-logged_in_user_id = None
+
 
 #functions
 def login():
@@ -20,12 +20,14 @@ def login():
     for u in users:
         if u.username == typed_username:
             if u.password == typed_password:
+                global logged_in_user_id
                 logged_in_user_id = u.id
                 homepage()
                 return  # exit the function once a matching user is found
     # display error message only after checking all users in the database
     click.echo("Incorrect username/password combo. Please try again!")
     login()
+
 def homepage():
     click.echo("What would you like to do?")
     options = ["Create Superlative", "View Popular Superlatives", "View Superlatives I've Created", "View What I've Been Nominated For"]
@@ -40,12 +42,12 @@ def homepage():
     elif menu_entry_index == 2:
         view_user_polls_created()
     elif menu_entry_index == 3:
-        # view_my_nominations()
-        homepage()
+        view_my_nominations()
+
 def create_superlative():
     superlative = click.prompt('Please state your superlative: ', type=str)
     click.echo(f"You have written: {superlative}.")
-    superlative_1 = Superlative(name = f'{superlative}', author_id = 1)
+    superlative_1 = Superlative(name = f'{superlative}', author_id = logged_in_user_id)
     session.add(superlative_1)
     session.commit()
     select_popular_unvoted()
@@ -62,6 +64,9 @@ def create_superlative():
 def select_popular_unvoted():
     # get all votes from votes table that were cast for the selected superlative
     click.echo("Here are superlatives that you haven't voted on yet, ranked by most total votes:")
+
+      # NEED TO FIX THIS TO MAKE IT FILTER BY NOT VOTED ON
+
     results = session.query(Superlative.name, func.count(Votes.superlative_id).label('total_votes')).outerjoin(Votes).group_by(Superlative.id).order_by(func.count(Votes.id).desc()).all()
     # Attempted refactoring
     options = []
@@ -85,14 +90,16 @@ def select_popular_unvoted():
         global selected_superlative_name, selected_superlative_id
         selected_superlative_name = options[menu_entry_index]
         selected_superlative_id = menu_entry_index + 1
+        # vote_on_superlative()
+        # summary_table()
+        # homepage() 
         vote_on_superlative()
-        summary_table()
-        homepage() 
-        vote_on_superlative(options[menu_entry_index], menu_entry_index+1)
 def select_recent_unvoted():
     # get all votes from votes table that were cast for the selected superlative
     # results = session.query(Superlative, Votes).filter(Superlative.id == Votes.superlative_id).all()
     click.echo("Here are superlatives that you haven't voted on yet, ranked by most recently created:")
+    
+    # NEED TO FIX THIS TO MAKE IT FILTER BY NOT VOTED ON
     results = session.query(Superlative).order_by(Superlative.date_created.desc()).all()
     # click.echo(f"{results}")
     options = []
@@ -120,40 +127,70 @@ def select_recent_unvoted():
         vote_on_superlative()
         summary_table()
         homepage() 
-        vote_on_superlative(options[menu_entry_index], menu_entry_index+1)
+        # vote_on_superlative(options[menu_entry_index], menu_entry_index+1)
     # pass
     # COPY AND PAST CODE FROM RECENTLY UNVOTED WITH CHANAGE TO SORTING
-def vote_on_superlative(superlative_name, superlative_id):
-    # KYUSHIK WRITING THIS CODE NOW
-    # click.echo(f"{superlative_name, superlative_id}")
-    click.echo(f"Kyushik is doing this part!")
-    options = []
-    options.append("***Go back to the homepage***")
-    terminal_menu = TerminalMenu(options)
-    menu_entry_index = terminal_menu.show()
-    if menu_entry_index == 0:
-        homepage()
+
 def get_superlative():
     click.echo("getting superlative...")
     superlative = session.query(Superlative.name).all()
     click.echo(superlative)
+
 def view_user_polls_created():
-    click.echo("Getting Polls that this user created")
+    click.echo("Getting Polls that you created")
     superlatives = session.query(Superlative).filter(Superlative.author_id == logged_in_user_id)
+    # click.echo(f"{logged_in_user_id}")
+
+
+
     options = []
+    
     for superlative in superlatives:
         options.append(f'{superlative.name}')
-    options.append("***Go back to the homepage***")
-    terminal_menu = TerminalMenu(options)
+    if len(options) == 0:
+        click.echo(f"You have not created any superlatives.")
+        
+    table_headers = ["Superlative Name"]
+    table_data = set(options)
+    print(tabulate(table_data, headers=table_headers))
+     
+    # options.append()
+    terminal_menu = TerminalMenu(["***Go back to the homepage***"])
     menu_entry_index = terminal_menu.show()
-    if menu_entry_index == len(options)-1:
+    if menu_entry_index == 0:
         homepage()
-    print(options[menu_entry_index])
-    # TO DECIDE WHERE THE NEXT PAGE IS
+
+def view_my_nominations():
+    click.echo("Getting Polls that you are nominated for")
+    # WORKING HERE
+    merged_data = session.query(Superlative, Votes)\
+                        .join(Votes, Superlative.id == Votes.superlative_id)\
+                        .filter(Votes.nominee_id == logged_in_user_id)\
+                        .all()
+    # click.echo(f"{logged_in_user_id}")
+    if not merged_data:
+        click.echo(f"You have not been nominated for any superlatives.")
+    options = []
+    for superlative, vote in merged_data:
+        options.append(f'{superlative.name}')
+
+    # print the results as a table
+    table_headers = ["Superlative Name"]
+    table_data = set(options)
+    print(tabulate(table_data, headers=table_headers))
+     
+    # options.append()
+    terminal_menu = TerminalMenu(["***Go back to the homepage***"])
+    menu_entry_index = terminal_menu.show()
+    if menu_entry_index == 0:
+        homepage()
+    
+
 def view_polls_user_not_voted():
     click.echo("Getting polls you have not voted on yet")
     # TO WRITE OUT THIS FUNCTION
     click.echo(datetime.now())
+
 def print(value):
     click.echo(value)
 def summary_table():
